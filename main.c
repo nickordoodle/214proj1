4a5e28f315585a99cf278b187bc31877b33c0f16
GriffinWood <gbw15@scarletmail.rutgers.edu>
2016-02-16 18:48:32 -0500

    Update main.c
    Updated compareInts who he told us to do it in class. Also input changes I made to main for testing.

--- main.c
+++ main.c
@@ -2,11 +2,15 @@
 #include "sorted-list.h"
 
 
-int compareInts(int first, int second){
+int compareInts(void* firstArg, void * secondArg){
+        int *firstInt = (int*)firstArg;
+        int *secondInt = (int*)secondArg;
+        int value = *firstInt-*secondInt;
 
-	return 0;
+        return value;
 
 }
+
 
 
 int compareFloats(float first, float second){
@@ -29,20 +33,48 @@ void destroyFunc(){
 
 	return;
 }
+/* helper function for easy insertion and deletion*/
+void change(SortedListPtr SLPtr, char * type, int * ptr){
+        int success  = -1;
+
+        if(*type == 'i' || *type == 'I')//insert check
+                success = SLInsert(SLPtr,ptr);
+        if(*type == 'r' || *type == 'R')
+                success = SLRemove(SLPtr,ptr);
+
+        printf("%s", type);
+
+        if(success == 1)
+                printf(" successful: ");
+        else if(success == -1)
+                printf("error\n");
+        else
+                printf(" unsuccessful: ");
+        printf("%d\n", ptr);
+
+
+}
 
 
 int main(int argc, char const *argv[])
 {
 
-	SortedListPtr SLPtr = SLCreate (compareInts, destroyFunc);
-       
-        SLInsert(SLPtr,value);
-     
+	int test = 1;
+        int test2 = 5;
+        int *value = NULL;
+        int *value2 = NULL;
+        int success = -1;
+        value = &test;
+        value2 = &test2;
 
+        SortedListPtr SLPtr = SLCreate (compareInts, destroyFunc);
+        //printf("%d\n",SLPtr->data);
+        //success = SLInsert(SLPtr,value);
 
-        printf("%d\n",SLRemove(SLPtr,value));
-
-
+        change(SLPtr, "Insert", value);
+        //printf("%d",SLInsert(SLPtr,value));
+        change(SLPtr, "Insert", value2);
+        change(SLPtr, "Remove", value);
 
 	return 0;
 }
